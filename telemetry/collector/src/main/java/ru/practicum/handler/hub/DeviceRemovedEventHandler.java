1af18873440f19a8db0d10837fab3289d88fb6bb
Александр Тужиков <alehandro.tuzhikov@gmail.com>
2026-03-25 16:23:25 +0300

    "feat: добавлен сервис Aggregator для агрегации событий датчиков в снапшоты"

--- telemetry/collector/src/main/java/ru/practicum/handler/hub/DeviceRemovedEventHandler.java
+++ telemetry/collector/src/main/java/ru/practicum/handler/hub/DeviceRemovedEventHandler.java
@@ -3,7 +3,7 @@ package ru.practicum.handler.hub;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import ru.practicum.kafka.KafkaProducer;
+import ru.practicum.kafka.EventProducer;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceRemovedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
@@ -15,7 +15,7 @@ import java.time.Instant;
 public class DeviceRemovedEventHandler implements HubEventHandler {
     @Value("${telemetry.hubs.v1.topic}")
     private String hubEventsTopic;
-    private final KafkaProducer producer;
+    private final EventProducer producer;
 
     @Override
     public HubEventProto.PayloadCase getMessageType() {
